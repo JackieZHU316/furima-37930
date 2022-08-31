d634c2aee7aa6ae18dbad5181e42136499d9c684
JackieZHU316 <zhumingjie316@gmail.com>
2022-08-31 23:50:58 +0800

    商品テーブルとActiveStorageのアソシエーションを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  # validates :name, presence: true
+  # validates :introduction, presence: true
+  # validates :category_id, presence: true
+  # validates :status_id, presence: true
+  # validates :charge_type_id, presence: true
+  # validates :region_id, presence: true
+  # validates :delivery_time_id, presence: true
+  # validates :price, presence: true
+
+  # belongs_to :user
+  has_one_attached :image
 end
