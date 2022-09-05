8bbad933685ecc65293e7846cf88540be9b3eb12
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-05 23:27:43 +0900

    バリエーション設定追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+
+  HANKAKU_NUMBER_REGEXP = /\A[0-9]+\z/
+
   validates :name, presence: true
   validates :introduction, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
@@ -6,7 +9,8 @@ class Item < ApplicationRecord
   validates :charge_type_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
   validates :region_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
   validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: HANKAKU_NUMBER_REGEXP }
+  validates :image, presence: true
 
 
   # belongs_to :user
@@ -18,5 +22,6 @@ class Item < ApplicationRecord
   belongs_to :charge_type
   belongs_to :region
   belongs_to :delivery_time
+
 
 end
