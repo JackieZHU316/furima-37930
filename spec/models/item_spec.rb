ee9ba1a82de989ef26ec0c7405e91c4dd9de9d69
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-06 15:56:49 +0900

    要らないテストコード削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -80,7 +80,6 @@ RSpec.describe Item, type: :model do
       it 'userが紐付いていなければ保存できない' do
         @item.user = nil
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include("User must exist")
       end
     end
