103df39adff3799e63501b8b2f1dadf1ce22c7d1
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-19 17:53:19 +0900

    マイグレーションファイルを編集する

--- db/migrate/20220919080632_create_contacts.rb
+++ db/migrate/20220919080632_create_contacts.rb
@@ -1,6 +1,13 @@
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
+      t.string :post_code,           null: false
+      t.integer :region_id,          null: false
+      t.string :city,                null: false
+      t.string :house_number,        null: false
+      t.string :building,            null: false
+      t.string :phone_number,        null: false
+      t.references :purchase,        null: false, foreign_key: true
 
       t.timestamps
     end
