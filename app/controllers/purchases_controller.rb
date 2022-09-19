9a2fd581589ab8e7b5fe1e735858737fa2c4c749
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-20 00:55:39 +0900

    contactテーブルとpurchaseテーブルにデータ保存できるように作業

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,23 @@ class PurchasesController < ApplicationController
   def index
     #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
     @purchase_contact = PurchaseContact.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @purchase_contact = PurchaseContact.new(purchase_params)
+    if @purchase_contact.valid?
+      @purchase_contact.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_contact).permit(:post_code, :region_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
--- app/models/purchase_contact.rb
+++ app/models/purchase_contact.rb
@@ -3,17 +3,17 @@ class PurchaseContact
   attr_accessor :post_code, :region_id, :city, :house_number, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
     validates :phone_number
     validates :user_id
-    validates :item_id
   end
   validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
-    # 各テーブルにデータを保存する処理を書く
+    purchase = Purchase.create(user_id: user_id, item_id: @item_id)
+    Contact.create(post_code: post_code, region_id: region_id, city: city, house_number: house_number, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 
 end
