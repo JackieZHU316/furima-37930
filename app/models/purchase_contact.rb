78be4117298b754113ce25aeb46e11bd220dbb09
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-23 21:09:52 +0900

    正規表現の修正

--- app/models/purchase_contact.rb
+++ app/models/purchase_contact.rb
@@ -3,10 +3,10 @@ class PurchaseContact
   attr_accessor :post_code, :region_id, :city, :house_number, :building, :phone_number, :user_id, :item_id
 
   with_options presence: true do
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code, format: {with: /\A[0-9]{3}+-+[0-9]{4}+\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :house_number
-    validates :phone_number
+    validates :phone_number, format: {with: /\A[0-9]{10,11}+\z/, message: "is invalid. You cannot include hyphen(-)"}
     validates :user_id
   end
   validates :region_id, numericality: {other_than: 1, message: "can't be blank"}
