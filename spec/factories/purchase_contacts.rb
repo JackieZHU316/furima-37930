95f55ea4c89f7bbbc733d829b7912c29bc7b340c
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-24 00:26:11 +0900

    トークンのテストコード

--- spec/factories/purchase_contacts.rb
+++ spec/factories/purchase_contacts.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     house_number  { '1-2' }
     building      { 'Super House' }
     phone_number  { '08012345678' }
-
+    token         {"tok_abcdefghijk00000000000000000"}
   end
 end
 
