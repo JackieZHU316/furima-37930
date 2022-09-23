998edb8b56c456198bb1243d13f2894653c9fdc0
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-23 21:09:19 +0900

    商品購入のためアクション追加の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,20 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :purchase_item
+
   def index
-    #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
-    @purchase_contact = PurchaseContact.new
-    @item = Item.find(params[:item_id])
+    @purchases = Purchase.all
+    if current_user.id != @item.user.id
+      @purchase_contact = PurchaseContact.new
+      @purchases.each do |purchase|
+        if @item.id == purchase.item_id
+          redirect_to root_path
+        end
+      end
+    else
+      redirect_to root_path
+    end
+
   end
 
   def create
@@ -19,6 +31,10 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_contact).permit(:post_code, :region_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def purchase_item
+    @item = Item.find(params[:item_id])
   end
 
 end
