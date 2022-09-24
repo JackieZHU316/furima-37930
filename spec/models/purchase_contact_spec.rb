b40a22968a728e5644cd0c15f40f5eb17243257d
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-24 22:48:38 +0900

    テストコード内のデータ処理を改善する

--- spec/models/purchase_contact_spec.rb
+++ spec/models/purchase_contact_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe PurchaseContact, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @purchase_contact = FactoryBot.build(:purchase_contact, user_id: user.id, item_id: item.id)
+      sleep(0.1)
     end
 
     context '商品配送先登録できるとき' do
