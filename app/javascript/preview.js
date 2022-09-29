6d843ff76cd5f778619383fc758b0dda89a950da
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-29 23:08:24 +0900

    プレビュー画像を表示するスペースを作成する

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   const itemForm = document.getElementById('new_item')
+  const previewList = document.getElementById('previews');
   if (!itemForm) return null;
   console.log("preview.jsが読み込まれました");
 });
