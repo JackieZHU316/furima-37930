669f010f0706bf9fb080db6f01ab09fb8f6b6e03
JackieZHU316 <zhumingjie316@gmail.com>
2022-09-29 23:02:44 +0900

    preview.jsが動作するように設定する

---
+++ app/javascript/preview.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const itemForm = document.getElementById('new_item')
+  if (!itemForm) return null;
+  console.log("preview.jsが読み込まれました");
+});
