cef7ee5190ee70d32dde457e38599b16642cffad
JackieZHU316 <zhumingjie316@gmail.com>
2022-10-14 00:42:31 +0900

    コメント投稿機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to "/items/#{params[:item_id]}"
+    else
+      @item = @comment.item
+      @comments = @item.comments
+      @purchases = Purchase.all
+      render "items/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:message).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
